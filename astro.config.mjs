0f121da09fcc1c99f4b63158e1a38f55b7d7b919
David Pustai <david.pustai@gmail.com>
2026-05-11 11:15:08 +0200

    Proxy fetch with Browserbase to prevent 429

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -8,6 +9,7 @@ export default defineConfig({
         process.env.NODE_ENV === "production"
             ? "https://ctverecky.djvesko.cz"
             : "http://localhost:3000",
+    adapter: vercel(),
     server: {
         port: 3000,
     },
